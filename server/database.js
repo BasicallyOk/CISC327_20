d4a80cfa481ace62ba8c24680eabb9ba4e0dff0d
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-10-05 02:13:14 -0400

    Implemented login functionality

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const dbURI = 'mongodb+srv://test:<password>@cluster0.bt5g3.mongodb.net/QBNB?retryWrites=true&w=majority'
+// TODO: use environment variable to allow for a separate testdb
+const dbURI = 'mongodb+srv://test:test@cluster0.bt5g3.mongodb.net/QBNB?retryWrites=true&w=majority'
 
 const connectDb = () => {
     return mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
