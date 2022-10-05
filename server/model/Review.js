31341c4ffcd0e883a40bb3d46f1a119fe0211880
Ammar <ammarlakdawala91@gmail.com>
2022-10-04 22:08:58 -0400

    Fixed EsLint pathing

--- server/model/Review.js
+++ server/model/Review.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+import mongoose from 'mongoose'
+const { Schema } = mongoose
 
-const Review = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema)
 
 const reviewSchema = new Schema({
-    listing: String,
-    user: String,
-    comments: [{ body: String, date: Date}],
-    date: {type: Date, default: Date.now},
-    hidden: Boolean,
-    meta: {
-        score: Number 
-    }
-});
+  listing: String,
+  user: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    score: Number
+  }
+})
