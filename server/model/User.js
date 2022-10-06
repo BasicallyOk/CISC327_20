8ae550ffae1558ed47ec080e2b1a563118fd655d
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-10-06 17:30:14 -0400

    added comments to user attributes

--- server/model/User.js
+++ server/model/User.js
@@ -1,27 +1,33 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
+  //email that is used to assign to user
   email: {
     type: String,
     required: true
   },
+  //username that is used to assign to user 
   username: {
     type: String,
     required: true
   },
+  //passowrd that is used to assign to user 
   password: {
     type: String,
     required: true
   },
+  //balance that is used to assign to user 
   balance: {
     type: Number,
     min: 1,
     required: true
   },
+  //billing address that is used to assign to user 
   billingAddress: {
     type: String,
     required: true
   },
+  //postal code that is used to assign to uer 
   postalCode: {
     type: String,
     required: true
