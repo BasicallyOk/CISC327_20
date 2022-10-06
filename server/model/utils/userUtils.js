37880c9e9d9f740b10889dda386cd6239d80cbd0
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-10-06 18:01:57 -0400

    created registration function

--- server/model/utils/userUtils.js
+++ server/model/utils/userUtils.js
@@ -6,11 +6,64 @@ const User = require('../User')
  * @returns
  */
 const validateEmail = (email) => {
+  // Validate if input is empty
+  if (email === '') {
+    return false
+  }
+  // Check if local part is longer than 64. Regex cant test this
+  if (email.split('@')[0].length > 64) {
+    return false
+  }
   return String(email)
     .toLowerCase()
     .match(
       /^[-a-z0-9~!$%^&*=+}{'?]+(\.[-a-z0-9~!$%^&*=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
     )
+}
+
+/**
+ * Validates password for registration
+ * @param {String} password 
+ */
+const validatePassword = (password) => {
+  //if password has length shorter than 6 then return false 
+  if (password.length < 6){
+    return false
+  }
+
+  //if password is all uppercase then return false 
+  if(password.toUpperCase() === password){
+    return false 
+  }
+  
+  //if password is all lowercase then return false 
+  if(password.toLowerCase() === password){
+    return false
+  }
+  
+  //checks if they are all alphanuermic and if there are special characters 
+  if (!password.match(/^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]$/)) {
+    return false
+  }
+  return true
+}
+
+const validateUsername = (username) => {
+  //return false if username is less than 2 characters and greater than 20 
+  if(username.length < 2 && username.length > 20){
+    return false
+  }
+ 
+  //return false if username contains anything otherthan alphanumeric values
+  if(!username.match(/^[0-9a-zA-Z]+$/)){
+    return false
+  }
+
+  //return false if username has a space in either prefix or suffix
+  if(username.startsWith(' ')|| username.endsWith(' ')) {
+    return false 
+  }
+  return true
 }
 
 /**
@@ -20,14 +73,10 @@ const validateEmail = (email) => {
  * @return {Object} true if the login operation succeeded and false if otherwise
  */
 async function login (email, password) {
-  // Validate if input is empty
-  if (email === '' || password === '') {
-    return null
+  if (password = ""){
+    return false
   }
-  // Check if local part is longer than 64. Regex cant test this
-  if (email.split('@')[0].length > 64) {
-    return null
-  }
+
   if (!validateEmail(email)) {
     return null
   }
@@ -44,4 +93,50 @@ async function login (email, password) {
   })
 }
 
-module.exports = { login }
+
+
+/**
+ * 
+ * @param {String} email email used for registration
+ * @param {String} password password for the user 
+ * @param {String} username username for the user
+ * @param {String} billing_address billing address for the user
+ * @param {String} postal_code postal code of user
+ * @param {Number} balance the balance for the user (set at 100 as signup bonus)
+ */
+async function register(email, password, username, billing_address, postal_code) {
+  
+  //makes sure that the email is valid when registrating 
+  if (!validateEmail(email)) {
+    return false
+  }
+
+  //make sure that the password meets the requirments 
+  if (!validatePassword(password)){
+    return false 
+  }
+
+  //make sure that the username meets the requirements 
+  if (!validateUsername(username)){
+    return false 
+  }
+  //looks through the database if the email has been used then return false 
+  await User.findOne({ email }).then(user => {
+    if (user) {
+      return false
+    }
+  })
+
+  //return false if billing address is not empty
+  if(billing_address != ''){
+    return false
+  }
+  
+  //return false if postal code is not empty 
+  if(postal_code != ''){
+    return false
+  }
+
+  return true
+}
+module.exports = { login, register}
