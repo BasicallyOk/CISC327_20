e2c74cb831ff6d0c22dae3a7d8b3d69e43ccd43b
Drake Li <drakehcli@yahoo.ca>
2022-10-06 22:11:45 -0400

    update user profile along with testing

--- server/model/__test__/User.test.js
+++ server/model/__test__/User.test.js
@@ -1,5 +1,6 @@
 const User = require('../User')
 const { login } = require('../utils/userUtils')
+const { update } = require('../utils/userUtils')
 const { connectDb, disconnectDb } = require('../../database')
 
 describe('Login functionality', () => {
@@ -81,6 +82,153 @@ describe('Login functionality', () => {
         expect(user).not.toBeNull()
         expect(user.email).toBeEqual('test@gmail.com')
         expect(user.username).toBeEqual('testUser')
+      })
+    })
+  })
+})
+
+// testinf for update user profile
+describe('Update user profile functionality', () => {
+  // Set up testing
+  beforeAll(() => {
+    connectDb()
+    // Register the test@gmail.com account
+    const testUser = new User({
+      email: 'test@gmail.com',
+      username: 'testUser',
+      password: 'password',
+      balance: 100,
+      billingAddress: 'address',
+      postalCode: 'A1B 2C3'
+    })
+    testUser.save()
+  })
+  afterAll(async () => {
+    await User.findOneAndRemove({ email: 'test@gmail.com' })
+    disconnectDb()
+  })
+
+  // testing for invalid inputs
+  describe('Input validation', () => {
+    // testing for empty parameters
+    it('should not accept empty username, email, billingAddress and postalCode', () => {
+      // all empty
+      update('', '', '', '').then(user => {
+        expect(user).toBeNull()
+      })
+      // one empty
+      update('', 'test@gmail.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', '', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', '', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', '').then(user => {
+        expect(user).toBeNull()
+      })
+      // two empty
+      update('', '', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('', 'test@gmail.com', '', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('', 'test@gmail.com', 'address', '').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', '', '', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', '', 'address', '').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', '', '').then(user => {
+        expect(user).toBeNull()
+      })
+      // three empty
+      update('testUser', '', '', '').then(user => {
+        expect(user).toBeNull()
+      })
+      update('', 'test@gmail.com', '', '').then(user => {
+        expect(user).toBeNull()
+      })
+      update('', '', 'address', '').then(user => {
+        expect(user).toBeNull()
+      })
+      update('', 'test@gmail.com', '', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+    })
+    // testing for invalid username
+    it('should not accept an invalid username format', () => {
+      update('a', 'test@gmail.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('ab', 'test@gmail.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('abcdefghijklmnopqrst+x', 'test@gmail.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+    })
+    // testing for invalid email
+    it('should not accept an invalid email format', () => {
+      update('testUser', 'notanemail', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'notanemail@@gmail.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'a"b(c)d,e:f;g<h>i[j\\k]l@example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'just"not"right@example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'this is"not\\allowed@example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'this\\ still\\"not\\\\allowed@example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', '1234567890123456789012345678901234567890123456789012345678901234+x@example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'i_like_underscore@but_its_not_allowed_in_this_part.example.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'QA[icon]CHOCOLATE[icon]@test.com', 'address', 'A1B 2C3').then(user => {
+        expect(user).toBeNull()
+      })
+    })
+    // testing for invalid postalCode
+    it('should not accept an invalid username format', () => {
+      update('testUser', 'test@gmail.com', 'address', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', '0123456789').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', '!@#$%^&*()-_=+[]{};:,<>./?').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', 'ABC DEF').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', '123 456').then(user => {
+        expect(user).toBeNull()
+      })
+      update('testUser', 'test@gmail.com', 'address', '1A2 B3C').then(user => {
+        expect(user).toBeNull()
+      })
+    })
+    it('should not update a user that does not exist', () => {
+      // In a perfect world, the testing db used in CI will not have this
+      update('non-existent-username', 'non-existent@gmail.com', 'non-existent-billing-address', 'non-existent-postal-code').then(user => {
+        expect(user).toBeNull()
       })
     })
   })
--- server/model/utils/userUtils.js
+++ server/model/utils/userUtils.js
@@ -1,6 +1,15 @@
 const User = require('../User')
 
 /**
+ * Validate username using R3-4
+ * @param {String} username 
+ * @returns
+ */
+const validateUserName = (username) => {
+  return String(username)
+    .match(/^[a-zA-Z0-9][a-zA-Z0-9 ]+[a-zA-Z0-9]$/i)
+}
+/**
  * Validate email using the rfc5322 standard
  * @param {String} email
  * @returns
@@ -11,6 +20,25 @@ const validateEmail = (email) => {
     .match(
       /^[-a-z0-9~!$%^&*=+}{'?]+(\.[-a-z0-9~!$%^&*=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
     )
+}
+/**
+ * Validate billingAddress using number, street name, extension
+ * @param {String} billingAddress 
+ * @returns 
+ */
+const validateBillingAddress = (billingAddress) => {
+  return String(billingAddress)
+    .match(/^\d+ [ ]? [a-zA-Z]+ []? [a-zA-Z]+$/i)
+}
+/**
+ * Validate postalCode using R3-2 and R3-3
+ * @param {String} postalCode 
+ * @returns 
+ */
+const validatePostalCode = (postalCode) => {
+  return String(postalCode)
+    .toUpperCase()
+    .match(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ] [ ]?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i)
 }
 
 /**
@@ -43,5 +71,39 @@ async function login (email, password) {
     }
   })
 }
-
-module.exports = { login }
+// write user update pofile function async
+async function update (username, email, billingAddress, postalCode) {
+  // Validate if input is empty
+  if (username === '' || email === '' || billingAddress === '' || postalCode == '') {
+    return null
+  }
+  // Check if username is valid
+  if (username.length <= 2 || username.length >=20) {
+    return null
+  }
+  if (!validateUserName(username)) {
+    return null
+  }
+  // Check if local part is longer than 64. Regex cant test this
+  if (email.split('@')[0].length > 64) {
+    return null
+  }
+  if (!validateEmail(email)) {
+    return null
+  }
+  // Ckeck if billingAddress is valid
+  if (billingAddress.split(' ').length != 3) {
+    return null
+  }
+  if (!validateBillingAddress) {
+    return null
+  }
+  // Check if postalCode is valid
+  if (postalCode.split(' ').length != 2) {
+    return null
+  }
+  if (!validatePostalCode) {
+    return null
+  }
+}
+module.exports = { login, update }
