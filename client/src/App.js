8115a803933ee5f1d6795c50f32d33d52d3a12c3
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-10-19 14:34:51 -0400

    Register page fully funcional

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react'
-import logo from './logo.svg'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom'
 import './App.css'
+import RegisterUser from './components/RegisterUser'
+
+import axios from 'axios'
 
 function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+	return (
+		<Router>
+			<div>
+				{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="/register" element={<RegisterUser />} />
+				</Routes>
+			</div>
+		</Router>
+	)
+}
+
+function Main () {
+	return (
+		<div>
+			<h4>QBNB</h4>
+			<Link to={'register'}>Register</Link>
+			<button onClick={() => axios.get('http://localhost:5000/')}>Ping server</button>
+		</div>
+	)
 }
 
 export default App
