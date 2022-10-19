2842fe2a63f1340c51eadf7528cb10dc0f27d49c
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-10-19 14:34:31 -0400

    Set up server with express routing + controller

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { connectDb } = require('./database')
+
+// Import the routers
+const usersRouter = require('./routes/user')
+
+const port = 5000 // Server will listen to port 5000
+const app = express()
+
+// Configuration
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*') // allow requests from anyone
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH')
+  next()
+})
+
+// Configuring express to use body-parser as middle-ware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+async function main () {
+  connectDb()
+
+  console.log('Connected to Database')
+
+  // Ping server
+  app.get('/', (req, res) => res.send('Ping'))
+
+  // Set up express routers
+  app.use('/user', usersRouter)
+
+  // Set up server listener on port
+  app.listen(port, () => {
+    console.log(`This Server running on port ${port}`)
+  })
+}
+
+main()
