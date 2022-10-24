11667091ed21da42eb7b023dbff53238a0f19c1d
Drake Li <drakehcli@yahoo.ca>
2022-10-24 14:53:35 -0400

    commented

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router() // set up express router
 
 const listingUtils = require('../model/utils/listingUtils')
 
