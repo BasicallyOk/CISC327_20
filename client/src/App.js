1aefeaa280b8c56b8ea3b0fbe9270a87071d3420
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-10-25 16:09:45 -0400

    created login page and user profile and added react routing for them

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -7,18 +7,23 @@ import {
 } from 'react-router-dom'
 import './App.css'
 import RegisterUser from './components/RegisterUser'
+import LoginUser from './components/LoginUser'
+import UserProfile from './components/UserProfile'
 
 import axios from 'axios'
 
 function App () {
+	// token and setToken are the return values of the useState
+	const [user, setUser] = useState()
+
 	return (
 		<Router>
 			<div>
-				{/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
 				<Routes>
-					<Route path="/" element={<Main />} />
+					<Route path="/login" element={<LoginUser user={user} setUser={setUser}/>} />
 					<Route path="/register" element={<RegisterUser />} />
+					<Route path='/profile' element={<UserProfile user={user}/>}/>
+					<Route path="/" element={<Main />} />
 				</Routes>
 			</div>
 		</Router>
@@ -30,6 +35,7 @@ function Main () {
 		<div>
 			<h4>QBNB</h4>
 			<Link to={'register'}>Register</Link>
+			<Link to={'login'}>Login</Link>
 			<button onClick={() => axios.get('http://localhost:5000/')}>Ping server</button>
 		</div>
 	)
