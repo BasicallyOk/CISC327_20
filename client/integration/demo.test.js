a13e6b6f72ee39bcc347fa26fae6d54b1c4d8987
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-11-01 15:19:59 -0400

    Added demo integration testing

--- client/integration/demo.test.js
+++ client/integration/demo.test.js
@@ -1,13 +1,17 @@
-const { Builder, Browser } = require('selenium-webdriver')
+/**
+ * Tests if selenium is working as expected
+ */
 
-describe('Login', () => {
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+
+describe('Selenium Test', () => {
     let driver
 
     beforeAll(async () => {
         driver = await new Builder().forBrowser(Browser.CHROME).build()
     })
 
-    it('demo', async () => {
+    it('Google Search', async () => {
         await driver.get('https://www.google.com/ncr')
         await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
         await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
