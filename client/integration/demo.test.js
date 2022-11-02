85498453fa1fea86899016af405c0f19d5a20316
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-11-01 21:20:45 -0400

    Selenium tests, yay

--- client/integration/demo.test.js
+++ client/integration/demo.test.js
@@ -5,17 +5,17 @@
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
 
 describe('Selenium Test', () => {
-    let driver
+	let driver
 
-    beforeAll(async () => {
-        driver = await new Builder().forBrowser(Browser.CHROME).build()
-    })
+	beforeAll(async () => {
+		driver = await new Builder().forBrowser(Browser.CHROME).build()
+	})
 
-    it('Google Search', async () => {
-        await driver.get('https://www.google.com/ncr')
-        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
-        await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
-    })
+	it('Google Search', async () => {
+		await driver.get('https://www.google.com/ncr')
+		await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+		await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+	})
 
 	afterAll(() => driver && driver.quit())
 })
