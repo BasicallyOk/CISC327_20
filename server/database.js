716c946f5f8c63d847b08596bacbd45928429a24
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-11-03 14:49:50 -0400

    Sacrifice to the linter god

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({ path: '../.env' })
 
 // TODO: use environment variable to allow for a separate testdb
 const dbURI = process.env.DB_URI
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({ path: '../.env' })
 
 const bodyParser = require('body-parser')
 const { connectDb } = require('./database')
