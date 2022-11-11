234804a53af64d8822bf6256cd1d5cee17e09e4a
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-11-11 18:34:35 -0500

    created updateUserProfile

---
+++ client/src/components/UpdateUserProfile.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+function UpdateUserProfile (props) {
+	const [username, setUsername] = useState('')
+	const [billingAddress, setBillingAddress] = useState('')
+	const [postalCode, setPostalCode] = useState('')
+
+	const handleSubmit = () => {
+		axios.post('/user/update', {
+			username,
+			billingAddress,
+			postalCode,
+			email: props.user.email
+		}).then(res => {
+			console.log(res.data.success)
+		}).catch(e => {
+			console.log(e.response.data.error)
+			alert('Unable to login')
+		})
+	}
+
+	return (
+		<div style={{
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center'
+		}}>
+			<p>Update</p>
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				<p>Username</p>
+				<input
+					type = 'text'
+					id = 'userBox'
+					value = {username}
+					onChange = {(event) => setUsername(event.target.value)}
+				/>
+			</div>
+
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				<p>Billing Address</p>
+				<input
+					type = 'text'
+					id = 'billingBox'
+					value = {billingAddress}
+					onChange = {(event) => setBillingAddress(event.target.value)}
+				/>
+			</div>
+
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				<p>Postal Code</p>
+				<input
+					type= 'text'
+					id = 'postalBox'
+					value = {postalCode}
+					onChange = {(event) => setPostalCode(event.target.value)}
+				/>
+			</div>
+
+			<button
+				id = 'submitButton'
+				onClick={handleSubmit}
+			>
+				Update
+			</button>
+			<Link to={'..'}>User</Link>
+		</div>
+	)
+}
+
+export default UpdateUserProfile
