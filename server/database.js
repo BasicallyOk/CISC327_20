2169f4f739ee76bf7b9bcfaae637c5cc49516caf
Khoa Nguyen <dangkhoa81122@gmail.com>
2022-11-18 01:07:21 -0500

    Allows for local backend testing

--- server/database.js
+++ server/database.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 require('dotenv').config({ path: '../.env' })
 
 // TODO: use environment variable to allow for a separate testdb
-const dbURI = process.env.DB_URI
+let dbURI = process.env.DB_URI
+if (process.env.DOCKERIZED === 'no') {
+  dbURI = process.env.LOCAL_DB_URI
+} 
 
 const connectDb = () => {
   mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
