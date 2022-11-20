e818ac5f359ca87c7480d1f4cc799e7911f8b3d5
Drake Li <drakehcli@yahoo.ca>
2022-11-19 23:48:33 -0500

    wip fixed createListing.test.js

--- server/model/__test__/createListing.test.js
+++ server/model/__test__/createListing.test.js
@@ -3,11 +3,17 @@ const User = require('../User')
 const { createListing } = require('../controller/listingUtils')
 const { connectDb, disconnectDb } = require('../../database')
 
-const { readFileSync } = require('fs')
-
-function syncReadFile (filename) {
-  const contents = readFileSync(filename, 'utf-8')
-  const arr = contents.split(/\r?\n/)
+function syncReadFile () {
+  let arr = [];
+  const f = require('fs');
+  const readline = require('readline');
+  var user_file = '../../resources/Generic_SQLI.txt';
+  var r = readline.createInterface({
+    input : f.createReadStream(user_file)
+  });
+  r.on('line', function (text) {
+    arr.push(text);
+  });
   return arr
 }
 
@@ -19,7 +25,7 @@ afterAll(async () => {
   await disconnectDb()
 })
 
-describe('Listing functionality', () => {
+describe('create listing functionality', () => {
   let id
   beforeAll(async () => {
     const testUser = new User({
@@ -50,24 +56,24 @@ describe('Listing functionality', () => {
     })
   })
   describe('SQL injection', () => {
-    const arr = syncReadFile('/Users/drakeli/Desktop/cisc327/CISC327_20/server/resources/Generic_SQLI.txt')
+    const arr = syncReadFile()
     it('should not create listing based on price', async () => {
       let listing
-      for (let i = 0; i < arr.length(); i++) {
+      for (let i = 0; i < arr.length; i++) {
         listing = await createListing('title', 'This is a test description that should work as it is more than 20 chars', arr[i], Date.now(), id)
         expect(listing).toBe(false)
       }
     })
     it('should not create listing based on date', async () => {
       let listing
-      for (let i = 0; i < arr.length(); i++) {
+      for (let i = 0; i < arr.length; i++) {
         listing = await createListing('title', 'This is a test description that should work as it is more than 20 chars', 100, arr[i], id)
         expect(listing).toBe(false)
       }
     })
     it('should not create listing based on id', async () => {
       let listing
-      for (let i = 0; i < arr.length(); i++) {
+      for (let i = 0; i < arr.length; i++) {
         listing = await createListing('title', 'This is a test description that should work as it is more than 20 chars', 100, Date.now(), arr[i])
         expect(listing).toBe(false)
       }
